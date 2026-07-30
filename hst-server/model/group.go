abff69e15b056111a42ac09a5446a9c1e2eeb9be
zeelrupapara <jeel.r@hybridsolutions.com>
2026-07-30 16:05:41 +0530

    chore: alignment issue fix for group config

--- hst-server/model/group.go
+++ hst-server/model/group.go
@@ -393,7 +393,6 @@ var (
 	}
 )
 
-
 // Group is a config template row in hst.groups (path hierarchy).
 type Group struct {
 	GroupID  int64  `db:"group_id" json:"group_id"`
@@ -460,22 +459,24 @@ type GroupSymbol struct {
 	Path        string `db:"path" json:"path"`
 	ConfigIndex int32  `db:"config_index" json:"config_index"`
 
-	TradeMode                      *TradeMode         `db:"trade_mode" json:"trade_mode,omitempty"`
-	ExecMode                       *ExecMode          `db:"exec_mode" json:"exec_mode,omitempty"`
-	FillFlags                      *FillingFlags      `db:"fill_flags" json:"fill_flags,omitempty"`
-	ExpirFlags                     *ExpirationFlags   `db:"expir_flags" json:"expir_flags,omitempty"`
-	SpreadDiff                     *int32             `db:"spread_diff" json:"spread_diff,omitempty"`
-	SpreadDiffBalance              *int32             `db:"spread_diff_balance" json:"spread_diff_balance,omitempty"`
-	StopsLevel                     *int32             `db:"stops_level" json:"stops_level,omitempty"`
-	FreezeLevel                    *int32             `db:"freeze_level" json:"freeze_level,omitempty"`
-	VolumeMin                      *int64             `db:"volume_min" json:"volume_min,omitempty"`
-	VolumeMinExt                   *int64             `db:"volume_min_ext" json:"volume_min_ext,omitempty"`
-	VolumeMax                      *int64             `db:"volume_max" json:"volume_max,omitempty"`
-	VolumeMaxExt                   *int64             `db:"volume_max_ext" json:"volume_max_ext,omitempty"`
-	VolumeStep                     *int64             `db:"volume_step" json:"volume_step,omitempty"`
-	VolumeStepExt                  *int64             `db:"volume_step_ext" json:"volume_step_ext,omitempty"`
-	VolumeLimit                    *int64             `db:"volume_limit" json:"volume_limit,omitempty"`
-	VolumeLimitExt                 *int64             `db:"volume_limit_ext" json:"volume_limit_ext,omitempty"`
+	TradeMode         *TradeMode       `db:"trade_mode" json:"trade_mode,omitempty"`
+	ExecMode          *ExecMode        `db:"exec_mode" json:"exec_mode,omitempty"`
+	FillFlags         *FillingFlags    `db:"fill_flags" json:"fill_flags,omitempty"`
+	ExpirFlags        *ExpirationFlags `db:"expir_flags" json:"expir_flags,omitempty"`
+	SpreadDiff        *int32           `db:"spread_diff" json:"spread_diff,omitempty"`
+	SpreadDiffBalance *int32           `db:"spread_diff_balance" json:"spread_diff_balance,omitempty"`
+	StopsLevel        *int32           `db:"stops_level" json:"stops_level,omitempty"`
+	FreezeLevel       *int32           `db:"freeze_level" json:"freeze_level,omitempty"`
+
+	VolumeMin      *int64 `db:"volume_min" json:"volume_min,omitempty"`
+	VolumeMinExt   *int64 `db:"volume_min_ext" json:"volume_min_ext,omitempty"`
+	VolumeMax      *int64 `db:"volume_max" json:"volume_max,omitempty"`
+	VolumeMaxExt   *int64 `db:"volume_max_ext" json:"volume_max_ext,omitempty"`
+	VolumeStep     *int64 `db:"volume_step" json:"volume_step,omitempty"`
+	VolumeStepExt  *int64 `db:"volume_step_ext" json:"volume_step_ext,omitempty"`
+	VolumeLimit    *int64 `db:"volume_limit" json:"volume_limit,omitempty"`
+	VolumeLimitExt *int64 `db:"volume_limit_ext" json:"volume_limit_ext,omitempty"`
+
 	MarginFlags                    *SymbolMarginFlags `db:"margin_flags" json:"margin_flags,omitempty"`
 	MarginInitial                  *float64           `db:"margin_initial" json:"margin_initial,omitempty"`
 	MarginMaintenance              *float64           `db:"margin_maintenance" json:"margin_maintenance,omitempty"`
@@ -498,30 +499,34 @@ type GroupSymbol struct {
 	MarginCurrency                 *string            `db:"margin_currency" json:"margin_currency,omitempty"`
 	MarginLiquidity                *float64           `db:"margin_liquidity" json:"margin_liquidity,omitempty"`
 	MarginHedged                   *float64           `db:"margin_hedged" json:"margin_hedged,omitempty"`
-	SwapMode                       *SwapMode          `db:"swap_mode" json:"swap_mode,omitempty"`
-	SwapLong                       *float64           `db:"swap_long" json:"swap_long,omitempty"`
-	SwapShort                      *float64           `db:"swap_short" json:"swap_short,omitempty"`
-	SwapYearDay                    *int32             `db:"swap_year_day" json:"swap_year_day,omitempty"`
-	SwapFlags                      *SwapFlags         `db:"swap_flags" json:"swap_flags,omitempty"`
-	SwapRateSunday                 *float64           `db:"swap_rate_sunday" json:"swap_rate_sunday,omitempty"`
-	SwapRateMonday                 *float64           `db:"swap_rate_monday" json:"swap_rate_monday,omitempty"`
-	SwapRateTuesday                *float64           `db:"swap_rate_tuesday" json:"swap_rate_tuesday,omitempty"`
-	SwapRateWednesday              *float64           `db:"swap_rate_wednesday" json:"swap_rate_wednesday,omitempty"`
-	SwapRateThursday               *float64           `db:"swap_rate_thursday" json:"swap_rate_thursday,omitempty"`
-	SwapRateFriday                 *float64           `db:"swap_rate_friday" json:"swap_rate_friday,omitempty"`
-	SwapRateSaturday               *float64           `db:"swap_rate_saturday" json:"swap_rate_saturday,omitempty"`
-	RETimeout                      *int32             `db:"re_timeout" json:"re_timeout,omitempty"`
-	IECheckMode                    *InstantMode       `db:"ie_check_mode" json:"ie_check_mode,omitempty"`
-	IETimeout                      *int32             `db:"ie_timeout" json:"ie_timeout,omitempty"`
-	IESlipProfit                   *int32             `db:"ie_slip_profit" json:"ie_slip_profit,omitempty"`
-	IESlipLosing                   *int32             `db:"ie_slip_losing" json:"ie_slip_losing,omitempty"`
-	IEVolumeMax                    *int64             `db:"ie_volume_max" json:"ie_volume_max,omitempty"`
-	IEVolumeMaxExt                 *int64             `db:"ie_volume_max_ext" json:"ie_volume_max_ext,omitempty"`
-	IEFlags                        *int32             `db:"ie_flags" json:"ie_flags,omitempty"`
-	OrderFlags                     *OrderFlags        `db:"order_flags" json:"order_flags,omitempty"`
-	PermissionsFlags               *int32             `db:"permissions_flags" json:"permissions_flags,omitempty"`
-	PermissionsBookDepth           *int32             `db:"permissions_book_depth" json:"permissions_book_depth,omitempty"`
-	REFlags                        *RequestFlags      `db:"re_flags" json:"re_flags,omitempty"`
+
+	SwapMode          *SwapMode  `db:"swap_mode" json:"swap_mode,omitempty"`
+	SwapLong          *float64   `db:"swap_long" json:"swap_long,omitempty"`
+	SwapShort         *float64   `db:"swap_short" json:"swap_short,omitempty"`
+	SwapYearDay       *int32     `db:"swap_year_day" json:"swap_year_day,omitempty"`
+	SwapFlags         *SwapFlags `db:"swap_flags" json:"swap_flags,omitempty"`
+	SwapRateSunday    *float64   `db:"swap_rate_sunday" json:"swap_rate_sunday,omitempty"`
+	SwapRateMonday    *float64   `db:"swap_rate_monday" json:"swap_rate_monday,omitempty"`
+	SwapRateTuesday   *float64   `db:"swap_rate_tuesday" json:"swap_rate_tuesday,omitempty"`
+	SwapRateWednesday *float64   `db:"swap_rate_wednesday" json:"swap_rate_wednesday,omitempty"`
+	SwapRateThursday  *float64   `db:"swap_rate_thursday" json:"swap_rate_thursday,omitempty"`
+	SwapRateFriday    *float64   `db:"swap_rate_friday" json:"swap_rate_friday,omitempty"`
+	SwapRateSaturday  *float64   `db:"swap_rate_saturday" json:"swap_rate_saturday,omitempty"`
+
+	RETimeout *int32 `db:"re_timeout" json:"re_timeout,omitempty"`
+
+	IECheckMode    *InstantMode `db:"ie_check_mode" json:"ie_check_mode,omitempty"`
+	IETimeout      *int32       `db:"ie_timeout" json:"ie_timeout,omitempty"`
+	IESlipProfit   *int32       `db:"ie_slip_profit" json:"ie_slip_profit,omitempty"`
+	IESlipLosing   *int32       `db:"ie_slip_losing" json:"ie_slip_losing,omitempty"`
+	IEVolumeMax    *int64       `db:"ie_volume_max" json:"ie_volume_max,omitempty"`
+	IEVolumeMaxExt *int64       `db:"ie_volume_max_ext" json:"ie_volume_max_ext,omitempty"`
+	IEFlags        *int32       `db:"ie_flags" json:"ie_flags,omitempty"`
+
+	OrderFlags           *OrderFlags   `db:"order_flags" json:"order_flags,omitempty"`
+	PermissionsFlags     *int32        `db:"permissions_flags" json:"permissions_flags,omitempty"`
+	PermissionsBookDepth *int32        `db:"permissions_book_depth" json:"permissions_book_depth,omitempty"`
+	REFlags              *RequestFlags `db:"re_flags" json:"re_flags,omitempty"`
 }
 
 // GroupStatusFromFlags maps permission_flags → active|inactive for admin UI.
