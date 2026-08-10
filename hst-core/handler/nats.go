576acc90eabdf21ec3c6ffb2339ab2af69e1d8e1
zeelrupapara <jeel.r@hybridsolutions.com>
2026-08-10 12:39:27 +0530

    fix: a manager change reaches the engine's group masks at once, not at the next boot

--- hst-core/handler/nats.go
+++ hst-core/handler/nats.go
@@ -25,6 +25,7 @@ func (h *Handler) subscribe() error {
 		model.SubjectSystemSymbols,
 		model.SubjectSystemCommissions,
 		model.SubjectSystemRules,
+		model.SubjectSystemManagers,
 	} {
 		if err := h.Subscribe(subject, h.ConfigSystemEventHandler); err != nil {
 			return err
@@ -128,6 +129,10 @@ func (h *Handler) ConfigSystemEventHandler(msg *natscore.Msg) {
 	}
 	if err := h.LoadCommission(ctx); err != nil {
 		h.Log.Log(logger.TypeCfg, logger.CodeErr, "could not reload commissions", "error", err.Error())
+		return
+	}
+	if err := h.LoadManagerGroups(ctx); err != nil {
+		h.Log.Log(logger.TypeCfg, logger.CodeErr, "could not reload manager groups", "error", err.Error())
 		return
 	}
 
